03e8295ed9834d5641d740e1e55acfe13ad54128
J0SAL <joyalmeida321@gmail.com>
2023-04-01 20:09:31 +0530

    code for grabing contract address of user

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ### KJSCE 2023
+
+#### Tasks
+
+1. Develop Frontend
+2. Develop Backend (Smart Contract)
+3. Save image + data on ipfs
+4. Retrive data from ipfs
+5. NFT based Rewards
+6. Integrate backend and frontend
+7. Deploy Frontend
+8. Deploy SmartContract
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -3,10 +3,17 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { useMoralis } from "react-moralis";
 
 import { ConnectButton } from "web3uikit"
 
 function NavBar() {
+
+  const { isWeb3Enabled, account } = useMoralis();
+  if (isWeb3Enabled) {
+    console.log("signer: ", account);
+  }
+
   return (
     <>
       {["lg"].map((expand) => (
