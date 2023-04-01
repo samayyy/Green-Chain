72f59b3bfb27fa0afec405655eda37f8a619a283
J0SAL <joyalmeida321@gmail.com>
2023-04-01 14:23:09 +0530

    added deploy scripts

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,58 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL || "";
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "";
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 5,
+    },
+    polygon_mumbai: {
+      url: POLYGON_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 80001,
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+    ],
+  },
+
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      polygonMumbai: POLYGONSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
---
+++ backend/helper-hardhat-config.js
@@ -0,0 +1,34 @@
+const networkConfig = {
+  default: {
+    name: "hardhat",
+    blockConfirmations: 0,
+  },
+  31337: {
+    name: "localhost",
+    blockConfirmations: 0,
+  },
+  1: {
+    name: "mainnet",
+    blockConfirmations: 1,
+  },
+  5: {
+    name: "goerli",
+    blockConfirmations: 1,
+  },
+  80001: {
+    name: "polygon_mumbai",
+    blockConfirmations: 1,
+  },
+};
+
+const frontEndContractsFile = "../frontend/constants/contractAddresses.json";
+const frontEndAbiFile = "../frontend/constants/abi.json";
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+  frontEndContractsFile,
+  frontEndAbiFile,
+};
