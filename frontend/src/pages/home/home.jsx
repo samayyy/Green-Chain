66e724a908437f80ed94e282649b8b252b34733f
Aditya Bapat <adityahbapat@gmail.com>
2023-04-02 09:05:00 +0530

    accordian

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -108,27 +108,15 @@ function Home() {
               <Accordion.Item eventKey="0">
                 <Accordion.Header>How Does it Work ?</Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  Eco-Sustain is a Decentralized Solution aimed to solve the socio-environmental problems.
+                  Just post the problem and bring a positive change in the world.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="1">
                 <Accordion.Header>Why Should I Do it ?</Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  Cause if we will not change the society then who will ?
+                  The world should be a better place because we are born in it !
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
