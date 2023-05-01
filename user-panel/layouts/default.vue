60c8f6657494e10c810e252a3137160c837780c8
samayyy <samaykgada09@gmail.com>
2023-05-02 01:09:14 +0530

    changes

---
+++ user-panel/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>Greenchain &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      title: 'Greenchain',
+    }
+  },
+}
+</script>
